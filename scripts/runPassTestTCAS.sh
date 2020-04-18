34f213f98ba01851823d474803fa2d2ccae81150
sohah <husseinsoh@gmail.com>
2020-04-17 21:33:59 -0500

    adjusting heap size in shell scripts

--- scripts/runPassTestTCAS.sh
+++ scripts/runPassTestTCAS.sh
@@ -1,6 +1,6 @@
 # this is regression test for passing TCAS
 
-alias runDiscovery='LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/soha/git/ranger-discovery/lib TARGET_CLASSPATH_WALA=/home/soha/git/ranger-discovery/build/examples/ java -Djava.library.path=/home/soha/git/ranger-discovery/lib -Xmx12288m -ea -Dfile.encoding=UTF-8 -jar /home/soha/git_pass.jpf-core/build/RunJPF.jar '
+alias runDiscovery='LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/soha/git/ranger-discovery/lib TARGET_CLASSPATH_WALA=/home/soha/git/ranger-discovery/build/examples/ java -Djava.library.path=/home/soha/git/ranger-discovery/lib -Xmx4000m -ea -Dfile.encoding=UTF-8 -jar /home/soha/git_pass.jpf-core/build/RunJPF.jar '
 
 shopt -s expand_aliases
 
