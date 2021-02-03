274e08ac9fd2834c4c9f4357241cf55698ebc712
sohah <husse200@umn.edu>
2021-02-03 07:58:04 -0600

    adding script for removing enteries for the new expirement

---
+++ src/DiscoveryExamples/clearRunForNewExpirement.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+rm GPCA_Alarm/*/*LOR*
+rm GPCA_Alarm/*/*ROR*
+rm GPCA_Alarm/*/gpca*.txt
+rm -fr GPCA_Alarm/*/output/*
+
+rm GPCA_Infusion/*/*LOR*
+rm GPCA_Infusion/*/*ROR*
+rm GPCA_Infusion/*/infusion*.txt
+rm -fr GPCA_Infusion/*/output/*
+
+rm TCAS/*/*LOR*
+rm TCAS/*/*ROR*
+rm TCAS/*/tcas*.txt
+rm -fr TCAS/*/output/*
+
+
+rm WBS/*/*LOR*
+rm WBS/*/*ROR*
+rm WBS/*/wbs*.txt
+rm -fr WBS/*/output/*
+
