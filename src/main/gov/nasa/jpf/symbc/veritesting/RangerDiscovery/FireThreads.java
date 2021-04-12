26b02c243662f5da209ae2bd45b74547222d08b9
soha <soha@cs-soha-u.cs.umn.edu>
2021-04-11 21:02:23 -0500

    adding statistics for all threads

--- src/main/gov/nasa/jpf/symbc/veritesting/RangerDiscovery/FireThreads.java
+++ src/main/gov/nasa/jpf/symbc/veritesting/RangerDiscovery/FireThreads.java
@@ -1,7 +1,12 @@
 package gov.nasa.jpf.symbc.veritesting.RangerDiscovery;
 
+import gov.nasa.jpf.symbc.veritesting.RangerDiscovery.Util.DiscoveryUtil;
 import gov.nasa.jpf.symbc.veritesting.RangerDiscovery.mutation.MutationResult;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Queue;
 import java.util.concurrent.ExecutionException;
@@ -10,22 +15,49 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class FireThreads {
+    public static FileWriter fw;
+    public static BufferedWriter bw;
+    public static PrintWriter out;
+    public static boolean firstTime = true;
+
     public static void findRepairs(int mutationNum, Queue<MutationResult> repairPossibilities) {
+        String repairFileName = Config.folderName + "MultiThread_stats_" + Config.spec;
+        try {
+            if (firstTime) {
+                fw = new FileWriter(repairFileName, false);
+                firstTime = false;
+            } else
+                fw = new FileWriter(repairFileName, true);
+        } catch (IOException e) {
+            System.out.println("problem creating MultiThread_stats for " + Config.spec);
+            assert false;
+        }
+
+        bw = new BufferedWriter(fw);
+        out = new PrintWriter(bw);
 
         ArrayList<MutationResult> repairPossibilitiesArr = new ArrayList<>(repairPossibilities);
         int coreCount = Runtime.getRuntime().availableProcessors();
-        ExecutorService service = Executors.newFixedThreadPool(coreCount-1);
+        ExecutorService service = Executors.newFixedThreadPool(coreCount - 1);
 
         ArrayList<Future> futures = new ArrayList<>();
 
+        long mutantRepairTime = System.currentTimeMillis();
+
         //mutation number is the
-        for (int i = 0; i < repairPossibilitiesArr.size(); i++) {  //repairPossibilitiesArr.size()
+        for (int i = 0; i < 1; i++) {  //repairPossibilitiesArr.size()
             String threadJpfFile = Config.folderName + "threadJpf_" + mutationNum + "_" + i + ".jpf";
             futures.add(service.submit(new RepairTask(repairPossibilitiesArr.get(i), threadJpfFile, mutationNum, i)));
         }
 
         try {
             getAllFutures(futures); //block main thread until all has threads has finished.
+            mutantRepairTime = System.currentTimeMillis() - mutantRepairTime;
+            out.print(Config.spec + ",  ");
+            out.print("prop" + Config.prop + ",  ");
+            out.print((DiscoveryUtil.convertTimeToSecond(mutantRepairTime)));
+            out.println();
+            out.close();
         } catch (InterruptedException e) {
             assert false : "interruptedException was raised, something went wrong. Failing.";
         } catch (ExecutionException e) {
